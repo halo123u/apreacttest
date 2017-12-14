3e1bb23c70e2adb81f2da1ea7d3ef7f3ceeb9ea7
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-12-13 22:31:48 -0500

    add Banner to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
+        <div className="Banner">
+          <div className="Navbar">
+            <ul>
+              <li>Reactor</li>
+              <li className="rightItem">Log In</li>
+            </ul>
+          </div>
+          <h1>Hello, Welcome to Reactor</h1>
+        </div>
+
       </div>
     );
   }
