63cc89c8a26c8e141c1282752ca014cb38c55810
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-12-14 03:07:59 -0500

    move navigation to Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Navbar = (props) => {
+    return (
+    <div className="Navbar">
+        <ul>
+            <li>Reactor</li>
+            <li className="rightItem">Log In</li>
+        </ul>
+    </div>)
+    
+}
+
+export default Navbar;
