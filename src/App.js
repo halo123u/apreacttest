166ba928c225502e1cccb612cb7a9f7543484e34
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-12-14 02:51:03 -0500

    add newsLetter section

--- src/App.js
+++ src/App.js
@@ -79,6 +79,14 @@ class App extends Component {
           </div>
         </div>
 
+        <div className="newsLetter">
+          <h2>SUBSCRIBE TO NEWSLETTER</h2>
+          <form className="subForm">
+            <input className="subInput" type="email" placeholder=" Your Email"/>
+            <input className="subButton" type="button" value="Subscribe"/> 
+          </form>
+        </div>
+
       </div>
     );
   }
